567107439a20309ee0c08de17f98ba52b9b64202
FaysalMahmudSajan <faysalmahmud4300@gmail.com>
2026-03-31 01:41:08 +0600

    upload

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ Base.metadata.create_all(engine)
 app = FastAPI(title="Farm2Plate (Faturi)")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://fatirbinabdullah.github.io/Farmer2Plate/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
